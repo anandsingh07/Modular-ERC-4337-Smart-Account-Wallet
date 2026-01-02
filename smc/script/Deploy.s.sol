4c4342edc1663b5f3af2908732e02abdf9daeba0
Anand Singh <anandsinghas9777@gmail.com>
2026-01-03 01:13:58 +0530

    feat: deploy ERC-4337 EntryPoint and fund SmartAccount deposit

--- smc/script/Deploy.s.sol
+++ smc/script/Deploy.s.sol
@@ -1,20 +1,22 @@
-// SPDX-License-Identifer:MIT 
-pragma solidity ^0.8.23 ;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
 
 import "../lib/forge-std/src/Script.sol";
 import "../lib/account-abstraction/contracts/core/EntryPoint.sol";
+import "../lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import "../src/SmartAccount.sol";
 
 contract Deploy is Script {
     function run() external {
         uint256 deployerKey = vm.envUint("PRIVATE_KEY");
+        address deployer = vm.addr(deployerKey);
 
         vm.startBroadcast(deployerKey);
 
         EntryPoint entryPoint = new EntryPoint();
 
         SmartAccount account = new SmartAccount(
-            msg.sender,
+            deployer,
             IEntryPoint(address(entryPoint))
         );
 
